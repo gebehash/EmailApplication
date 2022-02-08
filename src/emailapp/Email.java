f2497c35638f361451e55990e3607bc98ff0d395
George Herășteanu <geo.marsy3@gmail.com>
2022-02-08 20:31:15 +0200

    Better oop

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,68 +1,110 @@
 package emailapp;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Scanner;
 
 import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
 
 public class Email {
-    private String address;
-    private String firstName, lastName;
-    private String department, company;
+    private final String address;
+    private final String firstName;
+    private final String lastName;
+    private String department;
+    private final String companyPrefix = "hashinc.com";
     private String password;
+    private String alternateMail;
     private int mailboxCapacity = -1;
+    private Scanner in = new Scanner(System.in);
 
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    String setDepartment() {
+        System.out.println("Choose a department: \n 1. Sales\n 2. Accounting\n 3. Development\n 0. none");
+        int option = in.nextInt();
+        in.nextLine();
+        switch (option) {
+            case 1:
+                return "sales";
+            case 2:
+                return "accounting";
+            case 3:
+                return "dev";
+            default: return "";
+        }
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public Email() {
+        System.out.print("Enter your first name: ");
+        this.firstName = in.nextLine();
 
-    public void setDepartment(String department) {
-        this.department = department;
-    }
+        System.out.print("Enter your last name: ");
+        this.lastName = in.nextLine();
 
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-
-    public void setMailboxCapacity(int mailboxCapacity) {
-        this.mailboxCapacity = mailboxCapacity;
-    }
-
-    public int getMailboxCapacity() {
-        return mailboxCapacity;
-    }
-
-
-    public void generateEmail() {
+        department = setDepartment();
         if (department.length() > 1)
-            address = toLowerCase(firstName + "." + lastName + "@" + department + "." + company + ".com");
-        else address = toLowerCase(firstName + "." + lastName + "@" + company + ".com");
+            address = toLowerCase(firstName + "." + lastName + "@" + department + "." + companyPrefix);
+        else address = toLowerCase(firstName + "." + lastName + "@" + companyPrefix);
+
         password = "RANDOM"; // TODO: Write Random Password Generator
+
+        System.out.println("This is your email address: " + address);
+        System.out.println("This is your randomly chosen password: " + password);
+        pressEnterToContinue();
     }
 
-    public String getEmailAddress() {
-        return address;
+    public void displayName() {
+        System.out.println("Your name is " + firstName + ' ' + lastName + '\n');
+        pressEnterToContinue();
+
     }
 
+    public void displayEmailAddress() {
+        System.out.println("Your email is " + address + ".\n");
+        pressEnterToContinue();
+    }
 
-    public String getName() {
-        return firstName + ' ' + lastName;
+    public void setMailboxCapacity() {
+        System.out.print("Set the mailbox capacity: ");
+        mailboxCapacity = in.nextInt();
+        in.nextLine();
+        System.out.println("You have successfully set a mailbox capacity of " + mailboxCapacity + "mb.");
+        pressEnterToContinue();
+    }
+
+    public void displayMailboxCapacity() {
+        if (mailboxCapacity == -1) {
+            System.out.println("It seems like you didn't set a mailbox capacity.\n");
+            setMailboxCapacity();
+        }
+        else {
+            System.out.println("You have a mailbox capacity of " + mailboxCapacity + "mb.");
+            pressEnterToContinue();
+        }
+
     }
 
     public boolean verifyPassword(String oldPassword){
         return Objects.equals(oldPassword, password);
     }
 
-    public void changePassword(String password) {
-        this.password = password;
+    public void changePassword() {
+        System.out.print("Enter your current password: ");
+        String pass = in.nextLine();
+        if (verifyPassword(pass)) {
+            System.out.print("Enter the new password: ");
+            password = in.nextLine();
+            System.out.println("You have successfully changed the password!\n");
+        }
+        else {
+            System.out.println("Invalid password!\n");
+        }
+        pressEnterToContinue();
     }
 
-    public String getPassword() {
-        return password;
+    public void pressEnterToContinue() {
+        System.out.println("Press ENTER to continue...");
+        in.nextLine();
     }
+
 }
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -1,6 +1,6 @@
 package emailapp;
 
-import java.util.Random;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class EmailApp {
@@ -12,26 +12,6 @@ public class EmailApp {
 
         Email email = new Email();
 
-        System.out.println("Email generator\n\n");
-
-        System.out.println("What is your first name?");
-        email.setFirstName(scanner.nextLine());
-
-        System.out.println("What is your last name?");
-        email.setLastName(scanner.nextLine());
-
-        System.out.println("What company do you work for?");
-        email.setCompany(scanner.nextLine());
-
-        System.out.println("What department?");
-        email.setDepartment(scanner.nextLine());
-
-        email.generateEmail();
-
-        System.out.println("This is your email address: " + email.getEmailAddress());
-        System.out.println("This is your randomly chosen password: " + email.getPassword());
-        System.out.println("\n");
-
         while (option != 'q') {
             System.out.println("Welcome to your account! Choose an option: \n");
             System.out.println(" 1. Display name\n 2. Display email\n 3. Display mailbox capacity\n 4. Change password\n 5. Change mailbox capacity\n\n q. Quit");
@@ -39,42 +19,23 @@ public class EmailApp {
 
             switch (option) {
                 case '1':
-                    System.out.println("Your name is " + email.getName() + '.');
+                    email.displayName();
                     break;
 
                 case '2':
-                    System.out.println("Your email is " + email.getEmailAddress() + '.');
+                    email.displayEmailAddress();
                     break;
 
                 case '3':
-                    if (email.getMailboxCapacity() == -1) {
-                        System.out.println("It seems like you didn't set a mailbox capacity.\n Enter the desired capacity: ");
-                        email.setMailboxCapacity(scanner.nextInt());
-                        scanner.nextLine();
-                        System.out.println("You have successfully set a mailbox capacity of " + String.valueOf(email.getMailboxCapacity()) + ".");
-                    }
-                    else {
-                        System.out.println("You have a mailbox capacity of " + String.valueOf(email.getMailboxCapacity()) + ".");
-                    }
+                    email.displayMailboxCapacity();
                     break;
 
                 case '4':
-                    System.out.print("Enter your current password: ");
-                    Scanner scanner2 = new Scanner(System.in);
-                    pass = scanner2.nextLine();
-                    if (email.verifyPassword(pass)) {
-                        System.out.print("Enter the new password: ");
-                        email.changePassword(scanner2.nextLine());
-                    }
-                    else {
-                        System.out.println("Invalid password!\n");
-                    }
+                    email.changePassword();
                     break;
 
                 case '5':
-                    System.out.println("Set the mailbox capacity: ");
-                    email.setMailboxCapacity(scanner.nextInt());
-                    scanner.nextLine();
+                    email.setMailboxCapacity();
                     break;
 
             }
